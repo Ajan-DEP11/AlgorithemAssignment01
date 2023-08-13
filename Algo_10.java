88d195c28acb3a666224758e6490c35e65e0dab3
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-08-14 02:16:56 +0530

    Second commit

--- Algo_10.java
+++ Algo_10.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Algo_10 {
+    private static final Scanner scaanner = new Scanner(System.in);
     public static void main(String[] args) {
+
+        String TeleNum;
+        do{
+            System.out.println("Enter the phone number");
+            TeleNum = scaanner.nextLine();
+            
+
+
+
+
+
+        }while(true);
+
+    
         
     }
 }
