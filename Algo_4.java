5366e15e0210e7788a77e5ccc024ef18f7b1fcd1
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-08-14 01:25:42 +0530

    Completed Task 4

--- Algo_4.java
+++ Algo_4.java
@@ -1,5 +1,28 @@
 public class Algo_4 {
     public static void main(String[] args) {
         
+        int[] nums = {10, 20, 30, 40, 50}; 
+
+        int x= nums.length-1 ;
+        int y =0;
+
+        if(nums.length%2==0){
+           y=1 ;}
+
+        for (int i=0;i<nums.length/2 -y; i++){
+             
+            int k=0;
+
+                  k = nums[i] ;
+            nums[i] = nums[x] ;
+            nums[x] = k ;
+            x--;
+         }
+
+        for (int i=0;i<nums.length;i++)
+        {
+            System.out.printf("%d ",nums[i]);
+        }
+        System.out.println();
     }
 }
