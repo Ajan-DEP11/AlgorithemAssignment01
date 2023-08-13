23f95fa6fde35616c643fe15826f6bb55ab0eb03
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-08-13 18:50:30 +0530

    Second commit

--- Algo1.java
+++ Algo1.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Algo1 {
+    private static final Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
+
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        scanner.nextLine();
+
+        if(num>0){
+            System.out.println("This is a positive number");
+        }
         
     }
 }
