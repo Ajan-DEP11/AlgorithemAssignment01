749a573fb26de2363f2933b1dfd6a74bcc3d3234
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-08-14 02:25:26 +0530

    Completed task 9

--- Algo_9.java
+++ Algo_9.java
@@ -1,5 +1,43 @@
+import java.util.Scanner;
+
 public class Algo_9 {
+    private static final Scanner scaanner = new Scanner(System.in);
     public static void main(String[] args) {
-        
+
+        String word ;
+
+        do{
+            System.out.printf("Enter a word : ");
+            word =scaanner.nextLine();
+
+            String[] words = word.split(" ") ;
+
+            if(words.length>1 || word.isBlank()) {
+                System.out.println("Enter only a word") ;
+                continue ;
+            }
+            char[] chars =word.toCharArray();
+
+            int j=chars.length-1 ;
+            int charCount=0;
+            for (int i=0;i<chars.length/2; i++){
+                if(chars[i] == chars[j]) {
+                     charCount++ ;
+                }
+                j--;
+                
+            }
+
+            if(charCount==chars.length/2){
+                System.out.println("word is palindrom") ;
+            }
+            else{
+                System.out.println("word is not palindrom") ;
+            }
+
+        }
+
+        while(word.isBlank()) ;
+
     }
 }
