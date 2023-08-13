737768cb3d31daa374b0c7177fcdaf44be70ee96
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-08-14 01:39:46 +0530

    Second commit

--- Algo_5.java
+++ Algo_5.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Algo_5 {
+    private static final Scanner scaanner = new Scanner(System.in);
     public static void main(String[] args) {
+
+        String text;
+
+        System.out.println("Enter the text: ");
+        text = scaanner.nextLine().strip();
+
         
     }
 }
