2dfdb3ca295432bddcd1138970770de94dd6c907
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-08-14 00:45:50 +0530

    Completed Task 3

--- Algo1.java
+++ Algo1.java
@@ -14,7 +14,25 @@ public class Algo1 {
 
         if(num>0){
             System.out.println("This is a positive number");
+            continue;
         }
+        int fib_num=2;
+        int i =1;
+        String output="0,1,";
+
+        do{
+
+        output += i ;
+        output += "," ;
+         int k =0 ;
+         k =fib_num ;
+         fib_num =fib_num + i ;
+         i =k ;
+
+         
+
+
+        }while(true);
     }while(true);
         
     }
--- Algo3.java
+++ Algo3.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class Algo3 {
+    private static final Scanner scaanner = new Scanner(System.in);
     public static void main(String[] args) {
+
+        int[] nums = {10, 20, 30, 40, 50}; 
+
+        int[] reverseNums =new int[nums.length];
+
+        int x =0;
+        for (int i=nums.length-1;i>-1;i--){
+        
+             reverseNums[x] =nums[i];
+             x++ ;
+        }
+
+        nums =reverseNums ;
+
+         for (int i=0;i<nums.length;i++)
+        {
+            System.out.printf("%d, ",nums[i]);
+          
+        }  
+        System.out.println();
         
     }
 }
+
